c53a2094dea66f9db3dc0d4ad6b6dac5074e2383
czy12345 <1253665625@qq.com>
2018-01-18 11:26:25 +0800

    添加模块入口点

---
+++ src/local/LocalModule.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Document } from './model/Document'
+import { ImageConfig } from './model/ImageConfig';
+import { AudioConfig } from './model/AudioConfig';
+import { VideoConfig } from './model/VideoConfig';
+import { Bucket } from './model/Bucket';
+import { Audio } from './model/Audio'
+import { Video } from './model/Video'
+import { Image } from './model/Image';
+import { File} from './model/File'
+const typeormOptions = require('./typeormConfig')
+
+@Module({
+  modules: [TypeOrmModule.forRoot([ImageConfig,AudioConfig,VideoConfig,Bucket,Image,File,Video,Audio,Document],typeormOptions)],  
+  controllers: [],
+  components: [],
+  exports:[]
+})
+
+export class LocalModule {}
