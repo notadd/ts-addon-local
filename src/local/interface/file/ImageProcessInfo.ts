534040c0e24248c211b28e288545a7a0f05a4447
czy12345 <1253665625@qq.com>
2018-01-19 15:06:55 +0800

    图片处理信息接口类型

---
+++ src/local/interface/file/ImageProcessInfo.ts
@@ -0,0 +1,53 @@
+
+
+class ResizeData{
+    scale:number
+    wscale:number
+    hscale:number
+    width:number
+    height:number
+    pixel:number
+}
+
+class Resize{
+    mode:string
+    data:ResizeData
+}
+
+class Tailor{
+    isBefore:boolean
+    width:number
+    height:number
+    x:number
+    y:number
+    gravity:string
+}
+
+class Blur{
+    redius:number
+    sigma:number
+}
+
+
+export class ImagePreProcessInfo{
+    resize?:Resize
+    tailor?:Tailor
+    watermark?:boolean
+    rotate?:number
+}
+
+
+export class ImagePostProcessInfo{
+    resize?:Resize
+    tailor?:Tailor
+    roundrect?:number
+    watermark?:boolean
+    rotate?:number
+    blur?:Blur
+    sharpen?:boolean
+    format?:string
+    lossless?:boolean
+    quality?:number
+    progressive?:boolean
+    strip?:boolean
+}
--- src/local/util/ImageProcessUtil.ts
+++ src/local/util/ImageProcessUtil.ts
@@ -30,7 +30,7 @@ export class ImageProcessUtil {
     }
 
     process(imagePath,imageProcessInfo){
-        
+
     }
 
 }
