ae885ca6a1efed665acbdada5d2328f2067ee1b4
czy12345 <1253665625@qq.com>
2018-01-22 15:51:04 +0800

    格式处理函数’

--- src/local/util/ImageProcessUtil.ts
+++ src/local/util/ImageProcessUtil.ts
@@ -8,26 +8,17 @@ import { SharpInstance } from 'sharp'
 import * as crypto from 'crypto'
 import * as path from 'path'
 import * as fs from 'fs'
+import { KindUtil } from './KindUtil';
 
 /* 图片处理工具类 */
 @Component()
 export class ImageProcessUtil {
     private readonly gravity: Set<string>
-    private readonly font: Map<string, string>
-    private readonly colorRegex: RegExp
-    private readonly borderRegex: RegExp
-    private readonly format: Set<string>
-    constructor() {
+    constructor(
+        private readonly kindUtil:KindUtil
+    ) {
         //重心集合，在裁剪与水印中使用
         this.gravity = new Set(['northwest', 'north', 'northeast', 'west', 'center', 'east', 'southwest', 'south', 'southeast'])
-        //字体集合，在文字水印中使用
-        this.font = new Map([['宋体', 'simsun'], ['黑体', 'simhei'], ['楷体', 'simkai'], ['隶书', 'simli'], ['幼圆', 'simyou'], ['仿宋', 'simfang'], ['简体中文', 'sc'], ['繁体中文', 'tc'], ['Arial', 'arial'], ['Georgia', 'georgia'], ['Helvetica', 'helvetica'], ['Times-New-Roman', 'roman']])
-        //验证RGB颜色的正则表达式
-        this.colorRegex = new RegExp('^[0-9A-F]{6}$', 'g')
-        //验证文字描边的正则表达式
-        this.borderRegex = new RegExp('^[0-9A-F]{8}$', 'g')
-        //图片后处理保存格式集合
-        this.format = new Set(['jpeg', 'png', 'webp'])
     }
 
     //获取指定图片、字节缓冲的图片元数据，参数可以为图片路径或者Buffer对象
@@ -558,5 +549,13 @@ export class ImageProcessUtil {
 
         }
         else throw new Error('锐化参数错误')
+    }
+
+    format(instance:SharpInstance,format:string){
+        if(this.kindUtil.isImage(format)){
+            instance.toFormat(format)
+        }else{
+            throw new Error('格式参数错误')
+        }
     }
 }
