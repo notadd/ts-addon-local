66a84ae8d4fb73b50a7389abf9d55c0723da3762
czy12345 <1253665625@qq.com>
2018-01-23 14:56:53 +0800

    下载预处理resolver

--- src/local/graphql/resolver/FileResolver.ts
+++ src/local/graphql/resolver/FileResolver.ts
@@ -12,7 +12,8 @@ import { Image } from '../../model/Image';
 import { File } from '../../model/File'
 import * as  path from 'path'
 
-/*文件Resolver，包含了文件下载预处理、上传预处理、下载、上传、获取单个文件url、获取多个文件信息以及url、删除文件等接口
+/*文件Resolver，包含了文件下载预处理、上传预处理、下载、上传、
+  获取单个文件url、获取多个文件信息以及url、根据url访问文件、删除文件等接口
 */
 
 @Resolver('File')
@@ -27,4 +28,54 @@ export class FileResolver {
         @InjectRepository(Bucket) private readonly bucketRepository: Repository<Bucket>) {
     }
 
+
+    /* 文件下载预处理接口
+     当客户端需要下载某个文件时使用
+     返回下载文件的方法、url
+     @Param bucket_name：文件所属空间名
+     @Param type：       上传文件扩展名，即文件类型
+     @Param name：       文件名
+     @Return data.code：状态码，200为成功，其他为错误
+             data.message：响应信息
+             data.url：下载时的url
+             data.method： 下载方法
+  */
+  @Query('downloadProcess')
+  async downloadProcess(req , body):Promise<any>{
+      let data = {
+        code:200,
+        message:'',
+        //下载文件使用get方法
+        method:'get',
+        url:''
+      }
+      console.log(req.host)
+      let {bucket_name,name,type} =body
+      if(!bucket_name || !name || !type){
+        data.message = '缺少参数'
+        return data
+      }
+      let bucket:Bucket = await this.bucketRepository.findOne({name:bucket_name})
+      //指定空间不存在
+      if(!bucket){
+        data.code = 401
+        data.message = '指定空间'+bucket_name+'不存在'
+        return
+      }
+      let kind
+      let file:File|Audio|Video|Image|Document
+      if(this.kindUtil.isImage(type)){
+        file = await this.imageRepository.findOne({name,type,bucketId:bucket.id})
+      }else{
+        //其他类型暂不支持
+      }
+      if(!file){
+        data.code = 402
+        data.message  = '指定文件'+name+'不存在'
+        return data
+      }
+      data.url += '/'+bucket.name+'/'+file.name+'.'+file.type
+      data.message = '获取下载预处理信息成功'
+      return data
+    }
 }
