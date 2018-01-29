85be14d9b7679531278c27141a8d02d6506118eb
czy12345 <1253665625@qq.com>
2018-01-29 18:40:21 +0800

    获取文件大小方法

--- src/local/util/FileUtil.ts
+++ src/local/util/FileUtil.ts
@@ -7,7 +7,7 @@ export class FileUtil {
 
     constructor() { }
 
-    async write(path: string, buffer: Buffer) {
+    async write(path: string, buffer: Buffer):Promise<void>{
         let ex: HttpException
         await new Promise((resolver, reject) => {
             fs.writeFile(path, buffer, (err) => {
@@ -24,11 +24,11 @@ export class FileUtil {
         }
     }
 
-    async deleteIfExist(path:string){
-        if(fs.existsSync(path)){
+    async deleteIfExist(path: string):Promise<void>{
+        if (fs.existsSync(path)) {
             let ex: HttpException
             await new Promise((resolver, reject) => {
-                fs.unlink(path,(err) => {
+                fs.unlink(path, (err) => {
                     if (err) {
                         reject(new HttpException('文件删除错误:' + err.toString(), 407))
                     }
@@ -43,6 +43,30 @@ export class FileUtil {
         }
     }
 
-   
+    //获取文件状态，一般只有一个size能言用
+    async size(path:string):Promise<number>{
+        if (fs.existsSync(path)) {
+            let ex: HttpException
+            let size
+            await new Promise((resolver, reject) => {
+                fs.stat(path, (err,stats) => {
+                    if (err) {
+                        reject(new HttpException('获取文件状态错误:' + err.toString(), 407))
+                    }
+                    size = stats.size
+                    resolver()
+                })
+            }).catch(err => {
+                ex = err
+            })
+            if (ex) {
+                throw ex
+            }
+            return size
+        }else{
+            return null
+        }
+
+    }
 
 } 
