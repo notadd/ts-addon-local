dd1a7083fc76d31fc545eceaf196caa0a2ac2d31
czy12345 <1253665625@qq.com>
2018-02-06 11:39:35 +0800

    rebuild

--- starter/app.module.ts
+++ starter/app.module.ts
@@ -1,7 +1,7 @@
 import { Module,MiddlewaresConsumer,NestModule,RequestMethod, } from '@nestjs/common';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { GraphQLModule , GraphQLFactory} from '@nestjs/graphql';
-import { LocalModule } from './src/local/LocalModule'
+import { LocalModule } from '../src/LocalModule'
 
 @Module({
   modules: [GraphQLModule,LocalModule]
@@ -13,7 +13,7 @@ export class ApplicationModule implements NestModule{
   constructor(private readonly graphQLFactory: GraphQLFactory){}
 
   configure(consumer: MiddlewaresConsumer) {
-    const typeDefs = this.graphQLFactory.mergeTypesByPaths('./src/local/graphql/type/**/*.types.graphql');
+    const typeDefs = this.graphQLFactory.mergeTypesByPaths('./src/**/*.types.graphql');
     const schema = this.graphQLFactory.createSchema({ typeDefs });
     consumer
       .apply(graphiqlExpress({ endpointURL: '/graphql' }))
