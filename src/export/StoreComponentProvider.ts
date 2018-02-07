6463b4e46e71dd90ede58cef8549181fbf781e0b
czy12345 <1253665625@qq.com>
2018-02-07 10:11:24 +0800

    delete temp file

--- src/export/StoreComponentProvider.ts
+++ src/export/StoreComponentProvider.ts
@@ -1,8 +1,8 @@
 import { ImagePostProcessInfo, ImagePreProcessInfo } from '../interface/file/ImageProcessInfo';
-import { HttpException, Component , Inject} from '@nestjs/common';
+import { HttpException, Component, Inject } from '@nestjs/common';
 import { ImageMetadata } from '../interface/file/ImageMetadata';
 import { ImageProcessUtil } from '../util/ImageProcessUtil';
-import { Repository ,Connection} from 'typeorm';
+import { Repository, Connection } from 'typeorm';
 import { TokenUtil } from '../util/TokenUtil';
 import { FileUtil } from '../util/FileUtil';
 import { KindUtil } from '../util/KindUtil';
@@ -20,8 +20,8 @@ class StoreComponent {
         @Inject(ImageProcessUtil) private readonly imageProcessUtil: ImageProcessUtil,
         @Inject('LocalModule.ImageRepository') private readonly imageRepository: Repository<Image>,
         @Inject('LocalModule.BucketRepository') private readonly bucketRepository: Repository<Bucket>
-    ) { 
-      this.kindUtil = new KindUtil()
+    ) {
+        this.kindUtil = new KindUtil()
     }
 
     async delete(bucketName: string, name: string, type: string): Promise<void> {
@@ -51,7 +51,9 @@ class StoreComponent {
         }
         await this.fileUtil.delete(realPath)
     }
+
     async upload(bucketName: string, rawName: string, base64: string): Promise<{ bucketName: string, name: string, type: string }> {
+        let tempPath: string = path.resolve(__dirname, '../', 'store', 'temp', (+new Date()) + '' + rawName)
         if (!bucketName || !rawName || !base64) {
             throw new HttpException('缺少参数', 400)
         }
@@ -62,51 +64,56 @@ class StoreComponent {
         if (!bucket) {
             throw new HttpException('指定空间' + bucketName + '不存在', 401)
         }
-        let tempPath: string = path.resolve(__dirname, '../', 'store', 'temp', (+new Date()) + '' + rawName)
         await this.fileUtil.write(tempPath, Buffer.from(base64, 'base64'))
         let metadata: ImageMetadata
         let type: string = rawName.substring(rawName.lastIndexOf('.') + 1)
         //根据文件种类
         let kind: string = this.kindUtil.getKind(type)
-        if (kind === 'image') {
-
-            let imagePostProcessInfo: ImagePostProcessInfo
-            let format = bucket.image_config.format || 'raw'
-            //根据不同的图片保存类型，处理并且存储图片，返回处理后元数据
-            if (format === 'raw') {
-                imagePostProcessInfo = { strip: true, watermark: false }
-            } else if (format === 'webp_damage') {
-                imagePostProcessInfo = { format: 'webp', strip: true, watermark: false }
-            } else if (format === 'webp_undamage') {
-                imagePostProcessInfo = { format: 'webp', lossless: true, strip: true, watermark: false }
-            }
-            metadata = await this.imageProcessUtil.processAndStore(tempPath, bucket, imagePostProcessInfo)
-            let image: Image = new Image()
-            image.bucket = bucket
-            image.raw_name = rawName
-            image.name = metadata.name
-            image.type = metadata.format
-            image.width = metadata.width
-            image.height = metadata.height
-            image.size = metadata.size
-            let isExist: Image = await this.imageRepository.findOne({ name: metadata.name, bucketId: bucket.id })
-            //只有指定路径图片不存在时才会保存
-            if (!isExist) {
-                try {
-                    await this.imageRepository.save(image)
-                } catch (err) {
-                    //保存图片出现错误，要删除存储图片
-                    await this.fileUtil.delete(path.resolve(__dirname, '../', 'store', bucket.name, image.name + '.' + image.type))
-                    throw new HttpException('水印图片保存失败' + err.toString(), 410)
+        try {
+            if (kind === 'image') {
+                let imagePostProcessInfo: ImagePostProcessInfo
+                let format = bucket.image_config.format || 'raw'
+                //根据不同的图片保存类型，处理并且存储图片，返回处理后元数据
+                if (format === 'raw') {
+                    imagePostProcessInfo = { strip: true, watermark: false }
+                } else if (format === 'webp_damage') {
+                    imagePostProcessInfo = { format: 'webp', strip: true, watermark: false }
+                } else if (format === 'webp_undamage') {
+                    imagePostProcessInfo = { format: 'webp', lossless: true, strip: true, watermark: false }
                 }
+                metadata = await this.imageProcessUtil.processAndStore(tempPath, bucket, imagePostProcessInfo)
+                let image: Image = new Image()
+                image.bucket = bucket
+                image.raw_name = rawName
+                image.name = metadata.name
+                image.type = metadata.format
+                image.width = metadata.width
+                image.height = metadata.height
+                image.size = metadata.size
+                let isExist: Image = await this.imageRepository.findOne({ name: metadata.name, bucketId: bucket.id })
+                //只有指定路径图片不存在时才会保存
+                if (!isExist) {
+                    try {
+                        await this.imageRepository.save(image)
+                    } catch (err) {
+                        //保存图片出现错误，要删除存储图片
+                        await this.fileUtil.delete(path.resolve(__dirname, '../', 'store', bucket.name, image.name + '.' + image.type))
+                        throw new HttpException('上传图片保存失败' + err.toString(), 410)
+                    }
+                }
+            } else {
+                //其他类型暂不支持
             }
-        } else {
-            //其他类型暂不支持
+        } catch (err) {
+            throw err
+        } finally {
+            //如果中间过程抛出了异常，要保证删除临时图片
+            await this.fileUtil.deleteIfExist(tempPath)
         }
-        await this.fileUtil.deleteIfExist(tempPath)
         return { bucketName, name: metadata.name, type: metadata.format }
     }
-    async getUrl(req:any,bucketName: string, name: string, type: string, imagePostProcessInfo: ImagePostProcessInfo): Promise<string> {
+
+    async getUrl(req: any, bucketName: string, name: string, type: string, imagePostProcessInfo: ImagePostProcessInfo): Promise<string> {
         //验证参数
         if (!bucketName || !name || !type) {
             throw new HttpException('缺少参数', 400)
@@ -115,28 +122,28 @@ class StoreComponent {
         if (!bucket) {
             throw new HttpException('指定空间' + bucketName + '不存在', 401)
         }
-        let url:string = req.protocol + '://' + req.get('host') + '/local/file/visit'
+        let url: string = req.protocol + '://' + req.get('host') + '/local/file/visit'
         //根据文件种类，查找、删除数据库
         let kind = this.kindUtil.getKind(type)
         if (kind === 'image') {
             let image: Image = await this.imageRepository.findOne({ name, bucketId: bucket.id })
             if (!image) {
-              throw new HttpException('指定图片' + name + '.' + type + '不存在', 404)
+                throw new HttpException('指定图片' + name + '.' + type + '不存在', 404)
             }
             //所有文件调用统一的拼接Url方法 
             url += '/' + bucketName + '/' + name + '.' + type
             //存储图片处理信息时
             if (imagePostProcessInfo) {
-              //拼接图片处理的查询字符串
-              url += '?imagePostProcessString=' + JSON.stringify(imagePostProcessInfo)
-              //私有空间要拼接token，token使用它之前的完整路径计算
-              if (bucket.public_or_private === 'private') {
-                url += '&token=' + this.tokenUtil.getToken(url, bucket)
-              }
+                //拼接图片处理的查询字符串
+                url += '?imagePostProcessString=' + JSON.stringify(imagePostProcessInfo)
+                //私有空间要拼接token，token使用它之前的完整路径计算
+                if (bucket.public_or_private === 'private') {
+                    url += '&token=' + this.tokenUtil.getToken(url, bucket)
+                }
             } else {
-              if (bucket.public_or_private === 'private') {
-                url += '?token=' + this.tokenUtil.getToken(url, bucket)
-              }
+                if (bucket.public_or_private === 'private') {
+                    url += '?token=' + this.tokenUtil.getToken(url, bucket)
+                }
             }
         } else {
             //其他类型暂不支持
@@ -147,9 +154,9 @@ class StoreComponent {
 
 export const StoreComponentProvider = {
     provide: 'StoreComponentToken',
-    useFactory: (kindUtil:KindUtil,fileUtil:FileUtil,tokenUtil:TokenUtil,imageProcessUtil:ImageProcessUtil,imageRepository:Repository<Image>,bucketRepository:Repository<Bucket>)=>{
-        return new StoreComponent(kindUtil,fileUtil,tokenUtil,imageProcessUtil,imageRepository,bucketRepository)
+    useFactory: (kindUtil: KindUtil, fileUtil: FileUtil, tokenUtil: TokenUtil, imageProcessUtil: ImageProcessUtil, imageRepository: Repository<Image>, bucketRepository: Repository<Bucket>) => {
+        return new StoreComponent(kindUtil, fileUtil, tokenUtil, imageProcessUtil, imageRepository, bucketRepository)
     },
-    inject:[KindUtil,FileUtil,TokenUtil,ImageProcessUtil,'LocalModule.ImageRepository','LocalModule.BucketRepository']
-   
+    inject: [KindUtil, FileUtil, TokenUtil, ImageProcessUtil, 'LocalModule.ImageRepository', 'LocalModule.BucketRepository']
+
 }
