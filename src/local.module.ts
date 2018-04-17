e62caf924f05442b84ea1978d967142c143ab61b
czy12345 <1253665625@qq.com>
2018-04-17 11:55:24 +0800

    add check directory task to onModuleInit method

--- src/local.module.ts
+++ src/local.module.ts
@@ -1,24 +1,25 @@
-import { Global, Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { FileController } from "./controller/file.controller";
 import { StoreComponentProvider } from "./export/store.component.provider";
+import { Global, Module, OnModuleInit, Inject } from "@nestjs/common";
 import { ConfigResolver } from "./graphql/resolver/config.resolver";
 import { FileResolver } from "./graphql/resolver/file.resolver";
-import { Audio } from "./model/audio.entity";
+import { FileController } from "./controller/file.controller";
+import { ImageProcessUtil } from "./util/image.process.util";
 import { AudioConfig } from "./model/audio.config.entity";
-import { Bucket } from "./model/bucket.entity";
-import { Document } from "./model/document.entity";
-import { File } from "./model/file.entity";
-import { Image } from "./model/image.entity";
 import { ImageConfig } from "./model/image.config.entity";
-import { Video } from "./model/video.entity";
 import { VideoConfig } from "./model/video.config.entity";
 import { ConfigService } from "./service/config.service";
 import { FileService } from "./service/file.service";
-import { FileUtil } from "./util/file.util";
-import { ImageProcessUtil } from "./util/image.process.util";
-import { KindUtil } from "./util/kind.util";
+import { Document } from "./model/document.entity";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Bucket } from "./model/bucket.entity";
 import { TokenUtil } from "./util/token.util";
+import { Audio } from "./model/audio.entity";
+import { Image } from "./model/image.entity";
+import { Video } from "./model/video.entity";
+import { FileUtil } from "./util/file.util";
+import { KindUtil } from "./util/kind.util";
+import { File } from "./model/file.entity";
+import * as path from "path";
 
 @Global()
 @Module({
@@ -53,5 +54,22 @@ import { TokenUtil } from "./util/token.util";
         StoreComponentProvider,
     ],
 })
-export class LocalModule {
+export class LocalModule implements OnModuleInit {
+
+    constructor(@Inject(FileUtil) private readonly fileUtil: FileUtil) { }
+
+    async onModuleInit() {
+        const storages: string = path.resolve(process.cwd(), "storages");
+        const local: string = path.resolve(process.cwd(), "storages", "local");
+        const temp: string = path.resolve(process.cwd(), "storages", "local", "temp");
+        if (!this.fileUtil.exist(storages)) {
+            await this.fileUtil.mkdir(storages);
+        }
+        if (!this.fileUtil.exist(local)) {
+            await this.fileUtil.mkdir(local);
+        }
+        if (!this.fileUtil.exist(temp)) {
+            await this.fileUtil.mkdir(temp);
+        }
+    }
 }
